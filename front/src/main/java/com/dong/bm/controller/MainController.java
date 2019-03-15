9ef5c0a971118f5dd1367f4b946b3d793318b89a
rlaehdgus <rlaehdgus529@naver.com>
2019-03-16 00:25:42 +0900

    tiles 셋팅 및 구현

--- front/src/main/java/com/dong/bm/controller/MainController.java
+++ front/src/main/java/com/dong/bm/controller/MainController.java
@@ -23,17 +23,9 @@ public class MainController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home() {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "Main/main";
+		return "main/main";
 	}
 	
 }
---
+++ front/src/main/webapp/WEB-INF/tiles/layout/bm_header.jsp
@@ -0,0 +1,4 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<div>
+	<h2>여긴 header</h2>
+</div>
